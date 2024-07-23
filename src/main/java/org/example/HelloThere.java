6f8d34e893b989dbfc28a6362a21e8275cca0ee9
rbrownGithub <rodalebrownstiegleredtech@outlook.com>
2024-07-23 14:45:31 -0400

    Second commit

---
+++ src/main/java/org/example/HelloThere.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.util.Scanner;
+
+public class HelloThere{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello there! How are you doing today?");
+        String response1 = scanner.nextLine();
+        System.out.println("Wow - Interesting! Tell me more!");
+        String response2 = scanner.nextLine();
+        System.out.println("Well, thanks for talking to me. Goodbye!");
+    }
+}
